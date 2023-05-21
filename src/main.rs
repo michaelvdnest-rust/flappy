1f58fc32717fd75fe728faf848b3bc69137296bd
Michael van der Nest <michaelvdnest@gmail.com>
2023-05-21 19:38:05 +0200

    Use bracket-lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+
+// Use everything from bracket-lib
+use bracket_lib::prelude::*;
+
 fn main() {
     println!("Hello, world!");
 }
